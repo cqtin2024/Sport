cd50845c90437729b2a8becfa9cebfddcf88905b
cqtin2024 <cqtin.2024@gmail.com>
2025-11-02 12:36:09 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+// Đảm bảo tạo thư mục src/app/
+export default function Home() {
+  return (
+    <main>
+      <h1 class="text-xl p-4">Dự án đã sẵn sàng để Deploy lên Vercel!</h1>
+    </main>
+  );
+}
